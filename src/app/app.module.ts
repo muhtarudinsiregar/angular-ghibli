c74f9e8a3272be0225bde1c77ec1d0dada02532d
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-01-08 13:48:07 +0700

    Added HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
